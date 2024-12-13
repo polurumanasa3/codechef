133f69f3861c1b620e8952879319bd93625e6cd6
polurumanasa3 <manasap142@gmail.com>
2024-12-13 01:53:13 -0800

    Add files via upload

---
+++ c.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class c
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if((X>=7)&&(X<=30))
+		{
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+	}
+}
+		
