6e69d76bb577636307eae7a237d3ef42967b8679
polurumanasa3 <manasap142@gmail.com>
2024-12-16 00:18:51 -0800

    Add files via upload

---
+++ minimum wage.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class minimum wage
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner Wage = new Scanner(System.in);
+        int X = Wage.nextInt();
+        if(X > 11){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+
+	}
+}
