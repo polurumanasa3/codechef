b0b247a2fee101a35a8767387ffd7a3363353d06
polurumanasa3 <manasap142@gmail.com>
2024-12-16 01:50:07 -0800

    Add files via upload

---
+++ thala for reason.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner Thala = new Scanner(System.in);
+        int N = Thala.nextInt();
+        if (N == 7){
+            System.out.print("THALA");
+        }
+        else{
+            System.out.print("SADGE");
+        }
+	
+
+	}
+}
