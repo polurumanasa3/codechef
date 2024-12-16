5486cddb2a140baad6815e77a7b16bb1adf0eda2
polurumanasa3 <manasap142@gmail.com>
2024-12-16 00:24:50 -0800

    Add files via upload

---
+++ writingspeed.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int totalTime=X*5;
+		if(totalTime>60){
+		    System.out.println("NO");
+		}
+		else{
+		    System.out.println("YES");
+		}
+	}
+
+
+	
+}
