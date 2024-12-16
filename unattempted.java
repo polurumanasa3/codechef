173701dc46c93c714c7792177e1d59db871b3995
polurumanasa3 <manasap142@gmail.com>
2024-12-16 01:11:57 -0800

    Add files via upload

---
+++ unattempted.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	 Scanner inp = new Scanner(System.in);
+      
+              int a=inp.nextInt();
+              int b=inp.nextInt();
+              System.out.println(a-b);
+          
+	
+
+	}
+}
