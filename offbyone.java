df32c81c278d7cfac0afe34966444a19a4e695b2
polurumanasa3 <manasap142@gmail.com>
2024-12-16 00:15:50 -0800

    Add files via upload

---
+++ offbyone.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class offbyone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println((a+b) + "1");
+
+	}
+}
