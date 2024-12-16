d6b181174c22b394c9cb74d95fa52c5ad805ab65
polurumanasa3 <manasap142@gmail.com>
2024-12-16 00:11:27 -0800

    Add files via upload

---
+++ 404notfound.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner in=new Scanner(System.in);
+		int X;
+		X=in.nextInt();
+		if(X==404)
+		System.out.println("Not Found");
+		else
+		System.out.println("Found");
+	
+
+	}
+}
